8c7cbbf53542693837046b4fd5c474dff6cd127b
Nick Whitelegg <nickw4426@gmail.com>
2023-03-29 19:50:06 +0100

    Add basic mapping example

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 - `wk2-node-sqlite3`: Week 2 solution using the alternative [node-sqlite3](https://github.com/TryGhost/node-sqlite3) API, which you can try if you cannot get better-sqlite3 working.
 - `wk3`: Standard week 3 solution.
 - `wk4`: Standard week 4 solution
+- `wk6`: Basic map from week 6 
 
 The examples assume that `wadsongs.db` is in the same folder as the server. You will need to manually copy the file into the appropriate folder from the top-level folder of this repository.
---
+++ wk6/index.mjs
@@ -0,0 +1,23 @@
+// Week 6 partial answer - basic map 
+
+const map = L.map("map1");
+
+const attrib="Map data copyright OpenStreetMap contributors, Open Database Licence";
+
+const tileLayer = L.tileLayer(
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            { attribution: attrib }
+).addTo(map);
+
+// New York lat 40.75 lon -74
+map.setView([40.75, -74], 14);
+
+// Handle map click event
+
+map.on("click", e => {
+    // create a position using the coordinates from the map click
+    const mapClickPos = [e.latlng.lat, e.latlng.lng];
+
+    // Add a marker at that position
+    L.marker(mapClickPos).addTo(map);
+});
